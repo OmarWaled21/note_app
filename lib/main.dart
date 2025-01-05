99c950dcf3cb11600ca9fd436c5301f556945459
OmarWaled21 <omar.waled484@gmail.com>
2025-01-05 23:12:47 +0200

    add consumer to addNote button sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app_training/constants/consts.dart';
+import 'package:note_app_training/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app_training/models/note_model.dart';
 import 'package:note_app_training/views/notes_view.dart';
 
@@ -17,14 +19,19 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
-        scaffoldBackgroundColor: const Color.fromARGB(255, 48, 48, 48),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AddNoteCubit>(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+          scaffoldBackgroundColor: const Color.fromARGB(255, 48, 48, 48),
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app_training/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app_training/helper/show_snack_bar.dart';
 import 'package:note_app_training/widgets/add_note_form.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
@@ -6,11 +12,24 @@ class AddNoteButtonSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-      child: SingleChildScrollView(
-        child: AddNoteForm(),
-      ),
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+        if (state is AddNoteFailure) {
+          log('faild');
+        } else if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+          showSnackBar(context, 'Success');
+        }
+      },
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+          child: ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading ? true : false,
+            child: const SingleChildScrollView(child: AddNoteForm()),
+          ),
+        );
+      },
     );
   }
 }
